a22ac7ff2b9bd4969a55f74d9ea0ad26b5a56e37
Vleis Walker <vleiswal@gmail.com>
2020-12-03 05:14:56 +0200

    email_verify

---
+++ lib/src/screens/verify.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class VerifyScreen extends StatefulWidget {
+  @override
+  _VerifyScreenState createState() => _VerifyScreenState();
+}
+
+class _VerifyScreenState extends State<VerifyScreen> {
+  final auth = FirebaseAuth.instance;
+  User user;
+  Timer timer;
+
+  @override
+  void initState() {
+    user = auth.currentUser;
+    print(user);
+    user.sendEmailVerification();
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      checkEmailVerified();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('An email has been send to ${user.email} Please verify'),
+      ),
+    );
+  }
+
+  Future<void> checkEmailVerified() async {
+    user = auth.currentUser;
+    await user.reload();
+    if (user.emailVerified) {
+      timer.cancel();
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomeScreen()));
+    }
+  }
+}
